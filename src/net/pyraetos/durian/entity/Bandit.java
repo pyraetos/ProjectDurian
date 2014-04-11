6e02772d91956ca22e9d0a39b75716638cddbe98
Pyraetos <pyraetos@pyraetos.net>
2014-04-10 22:40:57 -0500

    Signed-off-by: Pyraetos <pyraetos@pyraetos.net>

---
+++ src/net/pyraetos/durian/entity/Bandit.java
@@ -0,0 +1,20 @@
+package net.pyraetos.durian.entity;
+
+import net.pyraetos.util.Sys;
+
+public class Bandit extends MovingEntity implements Runnable{
+
+	public Bandit(double x, double y){
+		super(x, y, "bandit/south.png", false);
+		Sys.thread(this);
+	}
+
+	public void run(){
+		while(true){
+			double magnitude = Math.floor(4 * Math.random());
+			byte direction = (byte)Math.floor(4 * Math.random());
+			move(magnitude, direction);
+			Sys.sleep((long)(5000L * Math.random()));
+		}
+	}
+}
